5685ba9f45e8dcff9b4b4893b9141b10acffaf5b
Lex <pineapplepear55@gmail.com>
2024-01-08 12:12:58 +0300

    feat: align education section

--- src/components/minor/educationItem.tsx
+++ src/components/minor/educationItem.tsx
@@ -1,7 +1,11 @@
-export function EducationItem() {
+interface IEducationItemProps {
+  active: boolean;
+}
+export function EducationItem(props: IEducationItemProps) {
   return (
     <div className="edu__item">
-      <span className="edu__icon icon_main"></span>
+      <span className={`edu__symbol${props.active ? ' active' : ''} symbol symbol_circle`}></span>
+      <span className="edu__symbol symbol symbol_line"></span>
       <p className="edu__text text text_date">2019</p>
       <p className="edu__text text text_name">Place #1</p>
       <p className="edu__text text text_subtext">Subtext</p>
--- src/components/sections/education.tsx
+++ src/components/sections/education.tsx
@@ -5,7 +5,11 @@ export function Education() {
   return (
     <div className="edu">
       <h2 className="edu__h2 h2">Education</h2>
-      <EducationItem />
+      <div className="edu__list list">
+        <EducationItem active={true} />
+        <EducationItem active={false} />
+        <EducationItem active={false} />
+      </div>
     </div>
   );
 }
