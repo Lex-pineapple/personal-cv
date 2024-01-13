8d86c739a0fbf773c427809c9d5a0d238958896e
Lex <pineapplepear55@gmail.com>
2024-01-14 02:10:58 +0300

    feat: finish adaptability

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -5,6 +5,7 @@ export function Skills() {
   return (
     <div className="skills">
       <div className="skills_container">
+        <h2 className="skills__h2 h2">Skills</h2>
         <div className="skills__type-selector">
           <div className="item_selectable active">Hard Skills</div>
           <div className="item_selectable">Soft Skills</div>
