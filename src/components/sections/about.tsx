4954fa2ab3f7f3ee2f0c5202760e00a31c619050
Lex <pineapplepear55@gmail.com>
2024-01-14 23:21:54 +0300

    feat: add scroll animations to about

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,12 +1,24 @@
 import Placeholder from '@assets/Placeholder.png';
 import './About.scss';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export function About() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref1 });
+  const x = useTransform(scrollYProgress, [0, 1], [0, 300]);
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+
   return (
     <div className="about">
       <div className="about_container">
-        <h2 className="about__h2 h2">About me</h2>
-        <img src={Placeholder} alt="" className="about__img img" />
+        <motion.div ref={ref1} style={{ right: x }} className="about__h2-motion h2-motion">
+          <h2 className="about__h2 h2">About me</h2>
+        </motion.div>
+        <motion.div ref={ref2} style={{ opacity }} className="about__img-motion img-motion">
+          <img src={Placeholder} alt="" className="about__img img" />
+        </motion.div>
         <div className="about__text-container">
           <div className="about__text-line">
             <div className="dec_item"></div>
