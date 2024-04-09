05f122a53bf841f8b97e088401be3dffc6f9ec82
Lex <pineapplepear55@gmail.com>
2024-04-09 22:01:58 +0300

    feat: add scroll to top button functionality

--- src/components/minor/button-top.tsx
+++ src/components/minor/button-top.tsx
@@ -2,8 +2,13 @@ import { ArrowUpIcon } from '@components/svg/arrow-up-icon';
 
 export function ButtonTop() {
   return (
-    <div className="button button_top">
+    <button
+      type="button"
+      className="button button_top"
+      onClick={() => window.scrollTo(0, 0)}
+      aria-label="top"
+    >
       <ArrowUpIcon sectionName="button_top" />
-    </div>
+    </button>
   );
 }
